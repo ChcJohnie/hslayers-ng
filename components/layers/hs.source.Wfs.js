c18a2d4740a8aabf58d7674fe46c569860114de4
Jan Shanel <ChcJohnie@gmail.com>
2017-03-17 09:59:27 +0100

    Revert to Openlayers WFS formats, works only up to WFS 1.1.0

--- components/layers/hs.source.Wfs.js
+++ components/layers/hs.source.Wfs.js
@@ -1,38 +1,26 @@
 define(function(require) {
     var ol = require('ol');
     return function(options) {
-        if (typeof options.version == 'undefined') options.version = '1.0.0';
+        if (typeof options.version == 'undefined') options.version = '1.1.0';
         if (typeof options.hsproxy == 'undefined') options.hsproxy = true;
         if (typeof options.format == 'undefined') options.format = new ol.format.GeoJSON();
         options.projection = options.projection.toUpperCase();
-        if (typeof options.parser == 'undefined') options.parser = function(response) {
-            var features = [];
-            var gm = new ol.format.GML3();
-            for (var key in gm) {
-                if (key.indexOf("_PARSERS") > 0)
-                    gm[key]["http://www.opengis.net/gml/3.2"] = gm[key]["http://www.opengis.net/gml"];
-            }
-            $("member", response).each(function() {
-                var attrs = {};
-                var geom_node = $("geometry", this).get(0) || $("CP\\:geometry", this).get(0);
-                attrs.geometry = gm.readGeometryElement(geom_node, [{}]);
-                var feature = new ol.Feature(attrs);
-                features.push(feature);
-            });
-            return features;
+        if (typeof options.outputFormat == 'undefined') {
+            if (options.format instanceof ol.format.GeoJSON) 
+                options.outputFormat = 'json';
+            else options.outputFormat = 'gml3';
         }
-
+        
         var src = new ol.source.Vector({
-            format: options.format,
+            format: options.format,//options.format,
             loader: function(extent, resolution, projection) {
                 this.set('loaded', false);
-                this.clear();
+                //this.clear();
                 if (console) console.log("resolution", resolution);
                 var p = options.url + (options.url.indexOf('?') > 0 ? '&' : '?') +
                     'service=WFS&TYPENAME=' + options.typename + '&request=GetFeature&' +
                     'version=' + options.version + '&' +
-                    'SRSNAME=' + options.projection + '&outputFormat=geojson&' +
-                    'bbox=' + extent.join(',') + ',urn:ogc:def:crs:EPSG:6.3:3857';
+                    'SRSNAME=' + options.projection + '&outputFormat=' + options.outputFormat + '&bbox=' + extent.join(',') + ',urn:ogc:def:crs:EPSG:6.3:3857';
                 var url = options.hsproxy ? "/cgi-bin/hsproxy.cgi?toEncoding=utf-8&url=" + window.escape(p) : p;
 
                 $.ajax({
@@ -40,21 +28,25 @@ define(function(require) {
                         context: this
                     })
                     .done(function(response) {
-                        this.addFeatures(options.parser(response));
+                        this.addFeatures(options.format.readFeatures(response));
                         this.hasLine = false;
                         this.hasPoly = false;
                         this.hasPoint = false;
+                        var source = this;
                         angular.forEach(this.getFeatures(), function(f) {
                             if (f.getGeometry()) {
                                 switch (f.getGeometry().getType()) {
-                                    case 'LineString' || 'MultiLineString':
-                                        this.hasLine = true;
+                                    case 'LineString':
+                                    case 'MultiLineString':
+                                        source.hasLine = true;
                                         break;
-                                    case 'Polygon' || 'MultiPolygon':
-                                        this.hasPoly = true;
+                                    case 'Polygon':
+                                    case 'MultiPolygon':
+                                        source.hasPoly = true;
                                         break;
-                                    case 'Point' || 'MultiPoint':
-                                        this.hasPoint = true;
+                                    case 'Point':
+                                    case 'MultiPoint':
+                                        source.hasPoint = true;
                                         break;
                                 }
                             }
@@ -68,6 +60,7 @@ define(function(require) {
             },
             projection: options.projection,
             strategy: ol.loadingstrategy.bbox
+
         });
         src.defOptions = options;
         return src;
